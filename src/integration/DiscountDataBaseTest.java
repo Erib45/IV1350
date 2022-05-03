5dac26f68580f22207f20980a8d50a19ab259e96
Ensco <104724466+Ensco@users.noreply.github.com>
2022-05-03 17:27:01 +0200

    Add files via upload
    Im adding the dsicount database test. It will not work in test package

---
+++ src/integration/DiscountDataBaseTest.java
@@ -0,0 +1,77 @@
+package integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import DTO.ItemDTO;
+import DTO.SaleDTO;
+import model.Item;
+
+class DiscountDataBaseTest {
+	DiscountDatabase database = new DiscountDatabase();
+	int customerID;
+	SaleDTO saleDTO;
+	ArrayList<Item> itemsInSale = new ArrayList<Item>();
+	Item item;
+	Item anotherItem;
+	
+	@BeforeEach
+	void setUp() {
+	item = new Item(new ItemDTO(1, 15, 0.08f,"name1", "description1"),1);	
+	anotherItem = new Item(new ItemDTO(5, 25, 0.1f, "name2", "description2"),1);
+
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void testCheckDiscount() {
+		itemsInSale.add(item);
+		itemsInSale.add(item);
+		customerID=101; 
+		float expected =  0.98f;
+		float actual = database.checkDiscount( customerID,saleDTO = new SaleDTO(30,itemsInSale));
+		assertEquals(expected, actual, "CheckDiscount failed");
+	}
+	
+	@Test
+	void testCheckInvalidCustomerIDDiscount() {
+		itemsInSale.add(item);
+		itemsInSale.add(item);
+		customerID=107;  
+		int expected = 1;
+		float actual = database.checkDiscount( customerID,saleDTO = new SaleDTO(30,itemsInSale));
+		assertEquals(expected, actual, "CheckDiscount failed");
+	}
+	
+	@Test
+	void testCheckDiscount10ItemsAndCustomerID() {
+
+		for(int i = 0; i<10; i++)
+			itemsInSale.add(item);
+
+		customerID=101; 
+		float expected =  1f-0.1f-0.02f;
+		float actual = database.checkDiscount( customerID,saleDTO = new SaleDTO(30,itemsInSale));
+		assertEquals(expected, actual, "CheckDiscount failed");
+	}
+	@Test
+	void testCheckDiscount10Items() {
+
+		for(int i = 0; i<10; i++)
+			itemsInSale.add(item);
+
+		customerID=000; 
+		float expected =  1f-0.1f;
+		float actual = database.checkDiscount( customerID,saleDTO = new SaleDTO(30,itemsInSale));
+		assertEquals(expected, actual, "CheckDiscount failed");
+	}
+
+}
